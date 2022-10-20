e4c215bc009451115d7387d10a83a22579ae7c7d
wiseaidev <business@wiseai.dev>
2022-10-20 11:23:35 +0300

    added contacts api endpoints
    Signed-off-by: wiseaidev <business@wiseai.dev>

---
+++ app/contacts/crud.py
@@ -0,0 +1,154 @@
+import datetime
+import logging
+
+from app.auth.crud import (
+    find_existed_user,
+)
+from app.contacts.model import (
+    Contacts,
+)
+from app.users.model import (
+    Users,
+)
+from app.users.schemas import (
+    UserObjectSchema,
+)
+
+logger = logging.getLogger(__name__)
+from app.utils.session import (
+    database,
+)
+
+
+async def create_new_contact(contact_email: str, user_id: int):
+    contact = await find_existed_user(email=contact_email)
+    if not contact:
+        return {
+            "status_code": 400,
+            "message": "You can't add a non existing user to"
+            " your contact list!",
+        }
+    elif contact.id == user_id:
+        return {
+            "status_code": 400,
+            "message": "You can't add yourself to your contact list!",
+        }
+    query = """
+        SELECT
+          *
+        FROM
+          contacts
+        WHERE
+          user = :user_id
+        AND
+          contact = :contact_id
+    """
+    values = {"user_id": user_id, "contact_id": contact.id}
+    found_contact = await database.fetch_one(query, values=values)
+    if found_contact:
+        return {
+            "status_code": 400,
+            "message": f"{contact.first_name} already exist in your"
+            " contact list!",
+        }
+    # add contact_id to contact list
+    query = """
+        INSERT INTO contacts (
+          user,
+          contact,
+          creation_date
+        )
+        VALUES (
+          :user_id,
+          :contact_id,
+          :creation_date
+        )
+    """
+    values = {
+        "user_id": user_id,
+        "contact_id": contact.id,
+        "creation_date": datetime.datetime.utcnow(),
+    }
+    await database.execute(query, values=values)
+    results = {
+        "status_code": 201,
+        "message": f"{contact.first_name} has been added to your contact"
+        " list!",
+    }
+    return results
+
+
+async def get_contacts():
+    # get all contacts for each user.
+    query = """
+        SELECT
+          *
+        FROM
+          contacts
+        LEFT JOIN
+          users
+        ON
+          contacts.user= users.id
+        GROUP BY
+          contacts.id
+    """
+    contacts = await database.fetch_all(query)
+    results = {
+        "status_code": 200,
+        "result": contacts,
+    }
+    return results
+
+
+async def find_existed_user_contact(user_id: int):
+    query = "SELECT * FROM contacts WHERE user=:user_id"
+    values = {"user_id": user_id}
+    return await database.fetch_one(query, values=values)
+
+
+async def get_user_contacts(user_id: int):
+    user = await find_existed_user_contact(user_id)
+    if user:
+        # get all contacts for each user.
+        query = """
+            SELECT
+              *
+            FROM
+              contacts
+            LEFT JOIN
+              users
+            ON
+              contacts.contact= users.id
+            WHERE
+              contacts.user= :user_id
+        """
+        values = {"user_id": user_id}
+        contacts = await database.fetch_all(query, values=values)
+        results = {"status_code": 200, "result": contacts}
+        return results
+    return {"status_code": 400, "message": "User not found!"}
+
+
+async def search_user_contacts(search: str, user_id: int):
+    search = search.lower()
+    user = await find_existed_user_contact(user_id)
+    if user:
+        query = """
+            SELECT
+              *
+            FROM
+              contacts
+            LEFT JOIN
+              users
+            ON
+              contacts.contact= users.id
+            WHERE
+              contacts.user= :user_id
+            AND
+              INSTR(users.first_name, :search) > 0
+        """
+        values = {"user_id": user_id, "search": search}
+        return_results = await database.fetch_all(query, values=values)
+        results = {"status_code": 200, "result": return_results}
+        return results
+    return {"status_code": 400, "message": "User not found!"}
---
+++ app/contacts/router.py
@@ -0,0 +1,135 @@
+from fastapi import (
+    APIRouter,
+    Depends,
+)
+from typing import (
+    Union,
+)
+
+from app.auth.schemas import (
+    ResponseSchema,
+)
+from app.contacts.crud import (
+    create_new_contact,
+    get_contacts,
+    get_user_contacts,
+    search_user_contacts,
+)
+from app.contacts.model import (
+    Contacts,
+)
+from app.contacts.schemas import (
+    AddContact,
+    GetAllContactsResults,
+)
+from app.users.schemas import (
+    UserObjectSchema,
+)
+from app.utils.jwt_util import (
+    get_current_active_user,
+)
+
+router = APIRouter(prefix="/api/v1")
+
+
+@router.post(
+    "/contact",
+    response_model=ResponseSchema,
+    status_code=201,
+    name="contacts:add-contact",
+    responses={
+        201: {
+            "model": ResponseSchema,
+            "description": "Return a message that indicates a new user"
+            "has been added to a contact list.",
+        },
+        400: {
+            "model": ResponseSchema,
+            "description": "Return this response in case of non existing user"
+            " or an already existed one in the contact list.",
+        },
+    },
+)
+async def add_contact(
+    contact: AddContact,
+    currentUser: UserObjectSchema = Depends(get_current_active_user),
+):
+    """
+    Add new contact to an authenticated user contacts list.
+    """
+    results = await create_new_contact(contact.contact, currentUser.id)
+    return results
+
+
+@router.get(
+    "/contact",
+    status_code=200,
+    response_model=Union[GetAllContactsResults, ResponseSchema],
+    name="contacts:get-all-contacts",
+    responses={
+        200: {
+            "description": "A list of contacts for each user.",
+        },
+    },
+)
+async def get_all_contacts(
+    currentUser: UserObjectSchema = Depends(get_current_active_user),
+):
+    """
+    Get all contacts grouped by users.
+    """
+    results = await get_contacts()
+    return results
+
+
+@router.get(
+    "/contacts",
+    response_model=Union[GetAllContactsResults, ResponseSchema],
+    status_code=200,
+    name="contacts:get-all-user-contacts",
+    responses={
+        200: {
+            "model": GetAllContactsResults,
+            "description": "A list of contacts for each user.",
+        },
+        400: {
+            "model": ResponseSchema,
+            "description": "User not found.",
+        },
+    },
+)
+async def get_contacts_user(
+    currentUser: UserObjectSchema = Depends(get_current_active_user),
+):
+    """
+    Get all contacts for an authenticated user.
+    """
+    results = await get_user_contacts(currentUser.id)
+    return results
+
+
+@router.get(
+    "/contacts/users/search",
+    response_model=Union[GetAllContactsResults, ResponseSchema],
+    status_code=200,
+    name="contacts:search-for-contact",
+    responses={
+        200: {
+            "model": GetAllContactsResults,
+            "description": "A list of filtered contacts.",
+        },
+        400: {
+            "model": ResponseSchema,
+            "description": "User not found.",
+        },
+    },
+)
+async def search_contacts_user(
+    search: str,
+    currentUser: UserObjectSchema = Depends(get_current_active_user),
+):
+    """
+    Search for a contact given an authenticated user.
+    """
+    results = await search_user_contacts(search, currentUser.id)
+    return results
